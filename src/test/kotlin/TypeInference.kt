902e4c3b8a4474852d5f945c32c324c42c9f7b36
hellonaeunkim <hellonaeunkim@gmail.com>
2025-02-12 21:03:43 +0900

    Type Inference : 변수나 반환 타입을 명시하지 않아도 컴파일러가 자동으로 타입을 추론하는 기능

---
+++ src/test/kotlin/TypeInference.kt
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test
+
+class TypeInference {
+
+    @Test
+    fun typeInfer() {
+        val str = "kotlin"
+        val age = 30
+        val bool1 = true // or false
+        val bool2: Boolean = true
+        val double1 = 3.14
+        val double2: Double = 3.14
+    }
+}
